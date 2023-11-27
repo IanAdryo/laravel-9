bffb9bbbf06bc7b237615f614aa217f17f6609ec
IanAdryo <i.sanchez@adryo.com.mx>
2023-11-27 12:21:15 -0600

    fin clase 7

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <title>Blog</title>
 </head>
 <body>
-@include('partials.navigation')
-<h1>Inicio</h1>
+    @include('partials.navigation')
+<h1>Blog</h1>
 </body>
 </html>
